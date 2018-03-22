4b577968c8b0872eb2b029eb92068d5c5215ce65
김경록 <jangdori@gimgyeonglog-ui-macbook-pro.local>
2018-03-22 13:27:13 +0900

    MainListView) Create empty file.

---
+++ PianoNote/View/MainList/FolderCell.swift
@@ -0,0 +1,14 @@
+//
+//  FolderCell.swift
+//  PianoNote
+//
+//  Created by 김경록 on 2018. 3. 22..
+//  Copyright © 2018년 piano. All rights reserved.
+//
+
+import UIKit
+
+class FolderCell: UICollectionViewCell {
+    
+}
+
---
+++ PianoNote/ViewController/MainList/MainListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainListViewController.swift
+//  PianoNote
+//
+//  Created by 김경록 on 2018. 3. 22..
+//  Copyright © 2018년 piano. All rights reserved.
+//
+
+import UIKit
+
+class MainListViewController: UIViewController {
+    
+}
+
