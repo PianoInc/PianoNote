4f61b39a58c0cc0e9f8c3634177e702e6eb7d291
Kevin <Kevin_baba@naver.com>
2018-03-23 15:57:57 +0900

    [asset] 필요한 asset들 삽입

---
+++ PianoNote/Asset/Xib/AnimatableTextsView.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatableTextsView.swift
+//  PianoNote
+//
+//  Created by Kevin Kim on 23/03/2018.
+//  Copyright © 2018 piano. All rights reserved.
+//
+
+import UIKit
+
+class AnimatableTextsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ PianoNote/Asset/Xib/PianoSegmentControl.swift
@@ -0,0 +1,27 @@
+//
+//  PianoSegmentControl.swift
+//  PianoNote
+//
+//  Created by Kevin Kim on 21/03/2018.
+//  Copyright © 2018 piano. All rights reserved.
+//
+
+import UIKit
+
+class PianoSegmentControl: UIView {
+    
+    @IBOutlet var buttons: [UIButton]!
+    weak var animatableTextsView: AnimatableTextsView?
+    
+    @IBAction func tap(_ sender: UIButton) {
+        
+        buttons.forEach { (button) in
+            button.alpha = sender != button ? 0.2 : 1
+        }
+        
+        //TODO: animatableView에게 통신을 보내야함 효과에 대해서 넘겨줘야함
+        
+    }
+    
+
+}
