9aef63cd33e596f4ecbb43c636910cff803b4539
Kevin <Kevin_baba@naver.com>
2018-03-28 16:12:05 +0900

    [temp] 임시 주석 처리(타겟 만들면 풀 예정)

--- PianoNote/View/MainNote/FormManager/FormManager_mac.swift
+++ PianoNote/View/MainNote/FormManager/FormManager_mac.swift
@@ -6,18 +6,18 @@
 //  Copyright © 2018년 piano. All rights reserved.
 //
 
-import AppKit
-
-extension NSTextView {
-    
-    func insertBulletString(_ attrString: NSAttributedString) {
-        
-        guard let textStorage = textView.textStorage else { return }
-        let attrString = NSAttributedString(string: "\n", attributes: defaultAttributesWithoutParaStyle)
-        textStorage.insert(attrString, at: textView.selectedRange.location)
-        textStorage.replaceCharacters(in: textView.selectedRange, with: attrString)
-        textView.scrollRangeToVisible(textView.selectedRange)
-        
-    }
-}
+//import AppKit
+//
+//extension NSTextView {
+//    
+//    func insertBulletString(_ attrString: NSAttributedString) {
+//        
+//        guard let textStorage = textView.textStorage else { return }
+//        let attrString = NSAttributedString(string: "\n", attributes: defaultAttributesWithoutParaStyle)
+//        textStorage.insert(attrString, at: textView.selectedRange.location)
+//        textStorage.replaceCharacters(in: textView.selectedRange, with: attrString)
+//        textView.scrollRangeToVisible(textView.selectedRange)
+//        
+//    }
+//}
 
