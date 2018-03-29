9779aa05f4a15b9e8a1bffd074e438275b5b22a4
김경록 <jangdori@gimgyeonglog-ui-macbook-pro.local>
2018-03-29 14:59:44 +0900

    MainListView) Facebook Comment Cell 구현

---
+++ PianoNote/ViewController/LookAround/Interact/InteractDetailView/DRDetailCommentSection.swift
@@ -0,0 +1,113 @@
+//
+//  DRDetailCommentSection.swift
+//  PianoNote
+//
+//  Created by 김경록 on 2018. 3. 29..
+//  Copyright © 2018년 piano. All rights reserved.
+//
+
+import UIKit
+
+class DRDetailCommentSection: UITableViewHeaderFooterView {
+    
+    let contentsView = makeView(UIView()) {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 13
+        $0.layer.shadowRadius = 2.5
+        $0.layer.shadowOffset = CGSize(width: 0, height: 1)
+        $0.layer.shadowColor = UIColor(hex8: "56565626").cgColor
+    }
+    let portraitImage = makeView(UIImageView()) {
+        $0.layer.cornerRadius = 12.5
+        $0.contentMode = .scaleAspectFit
+    }
+    let nameLabel = makeView(UILabel()) {
+        $0.textColor = UIColor(hex6: "365899")
+        $0.font = UIFont.preferred(font: 13, weight: .bold)
+    }
+    let contentLabel = makeView(UILabel()) {
+        $0.numberOfLines = 0
+        $0.font = UIFont.preferred(font: 15, weight: .regular)
+    }
+    
+    let toolView = UIView()
+    let arrowImage = makeView(UIImageView()) {
+        $0.contentMode = .scaleAspectFit
+    }
+    let replyLabel = makeView(UILabel()) {
+        $0.font = UIFont.preferred(font: 12, weight: .regular)
+    }
+    let timeLabel = makeView(UILabel()) {
+        $0.font = UIFont.preferred(font: 12, weight: .regular)
+    }
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        backgroundColor = .clear
+        contentView.addSubview(contentsView)
+        contentsView.addSubview(portraitImage)
+        contentsView.addSubview(nameLabel)
+        contentsView.addSubview(contentLabel)
+        contentView.addSubview(toolView)
+        toolView.addSubview(arrowImage)
+        toolView.addSubview(replyLabel)
+        toolView.addSubview(timeLabel)
+        initConst()
+    }
+    
+    private func initConst() {
+        func constraint() {
+            makeConst(contentsView) {
+                $0.leading.equalTo(self.minSize * 0.0213)
+                $0.trailing.equalTo(-(self.minSize * 0.0213))
+                $0.top.equalTo(0)
+                $0.bottom.equalTo(-(self.minSize * 0.0933))
+            }
+            makeConst(portraitImage) {
+                $0.leading.equalTo(self.minSize * 0.0266)
+                $0.top.equalTo(self.minSize * 0.016)
+                $0.width.equalTo(25)
+                $0.height.equalTo(25)
+            }
+            makeConst(nameLabel) {
+                $0.leading.equalTo(self.portraitImage.snp.trailing).offset(self.minSize * 0.0266)
+                $0.trailing.equalTo(-(self.minSize * 0.0266))
+                $0.top.equalTo(self.portraitImage.snp.top)
+                $0.height.equalTo(25)
+            }
+            makeConst(contentLabel) {
+                $0.leading.equalTo(self.minSize * 0.0346)
+                $0.trailing.equalTo(-(self.minSize * 0.0346))
+                $0.top.equalTo(self.portraitImage.snp.bottom).offset(self.minSize * 0.0133)
+                $0.bottom.equalTo(-(self.minSize * 0.0346))
+                $0.height.greaterThanOrEqualTo(0)
+            }
+            makeConst(toolView) {
+                $0.leading.equalTo(0)
+                $0.trailing.equalTo(0)
+                $0.top.equalTo(self.contentsView.snp.bottom)
+                $0.bottom.equalTo(0)
+            }
+            makeConst(arrowImage) {
+                $0.leading.equalTo(self.minSize * 0.072)
+                $0.top.equalTo(self.minSize * 0.0266)
+                $0.width.equalTo(15)
+                $0.height.equalTo(15)
+            }
+            makeConst(replyLabel) {
+                $0.leading.equalTo(self.arrowImage.snp.trailing).offset(self.minSize * 0.0266)
+                $0.top.equalTo(0)
+                $0.bottom.equalTo(0)
+            }
+            makeConst(timeLabel) {
+                $0.leading.equalTo(self.replyLabel.snp.trailing).offset(self.minSize * 0.0266)
+                $0.top.equalTo(0)
+                $0.bottom.equalTo(0)
+            }
+        }
+        constraint()
+        device(orientationDidChange: { _ in constraint()})
+    }
+    
+}
+
--- PianoNote/ViewController/LookAround/Interact/InteractDetailView/InteractDetailViewController.swift
+++ PianoNote/ViewController/LookAround/Interact/InteractDetailView/InteractDetailViewController.swift
@@ -10,32 +10,29 @@ import UIKit
 
 class InteractDetailViewController: UIViewController {
     
-    @IBOutlet private var titleLabel: UILabel! { didSet {
-        titleLabel.font = UIFont.preferred(font: 23, weight: .bold)
+    @IBOutlet private var listView: UITableView! { didSet {
+        listView.register(DRDetailCommentSection.self, forHeaderFooterViewReuseIdentifier: "DRDetailCommentSection")
+        listView.contentInset.bottom = minSize * 0.3413
+        listView.initHeaderView(minSize * 0.2666)
+        listView.sectionHeaderHeight = UITableViewAutomaticDimension
+        listView.estimatedSectionHeaderHeight = 100
+        listView.rowHeight = UITableViewAutomaticDimension
+        listView.estimatedRowHeight = 100
         }}
-    @IBOutlet private var listView: UITableView!
     
     var data = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = "존경하는 아이폰 유저님들! 평소에 메모앱에다가 무엇을 적으시나요?"
         initConst()
     }
     
     private func initConst() {
         func constraint() {
-            makeConst(titleLabel) {
-                $0.leading.equalTo(self.safeInset.left + self.minSize * 0.1093).priority(.high)
-                $0.trailing.equalTo(-(self.safeInset.right + self.minSize * 0.1093)).priority(.high)
-                $0.top.equalTo(self.statusHeight + self.naviHeight + self.minSize * 0.064).priority(.high)
-                $0.width.lessThanOrEqualTo(limitWidth).priority(.required)
-                $0.centerX.equalToSuperview().priority(.required)
-            }
             makeConst(listView) {
                 $0.leading.equalTo(self.safeInset.left).priority(.high)
                 $0.trailing.equalTo(-self.safeInset.right).priority(.high)
-                $0.top.equalTo(self.titleLabel.snp.bottom).offset(self.minSize * 0.064).priority(.high)
+                $0.top.equalTo(self.statusHeight + self.naviHeight).priority(.high)
                 $0.bottom.equalTo(-self.safeInset.bottom).priority(.high)
                 $0.width.lessThanOrEqualTo(limitWidth).priority(.required)
                 $0.centerX.equalToSuperview().priority(.required)
@@ -45,29 +42,72 @@ class InteractDetailViewController: UIViewController {
         device(orientationDidChange: { _ in constraint()})
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        _ = dispatchOnce
+    }
+    
+    /// One time dispatch code.
+    private lazy var dispatchOnce: Void = {
+        // DetailView에서는 기존의 모습과 다른 모습이라 titleLabel을 재정의 한다.
+        if let headerView = listView.tableHeaderView as? DRNoteCellHeader {
+            headerView.contentView.subviews.forEach {$0.isHidden = true}
+            headerView.contentView.titleLabel.isHidden = false
+            headerView.contentView.titleLabel.numberOfLines = 2
+            headerView.contentView.titleLabel.font = UIFont.preferred(font: 23, weight: .bold)
+            makeConst(headerView.contentView.titleLabel) {
+                $0.leading.equalTo(self.minSize * 0.1066)
+                $0.trailing.equalTo(-(self.minSize * 0.1066))
+                $0.top.equalTo(self.minSize * 0.0666)
+                $0.bottom.equalTo(-(self.minSize * 0.0666))
+            }
+            headerView.contentView.titleLabel.text = "존경하는 아이폰 유저님들! 평소에 메모앱에다가 무엇을 적으시나요?"
+        }
+        // TableView의 section auto layout을 적용하기 위한 코드.
+        listView.beginUpdates()
+        listView.endUpdates()
+        navigationItem.titleView = makeView(UILabel()) {
+            $0.font = UIFont.preferred(font: 17, weight: .semibold)
+            $0.text = "존경하는 아이폰 유저님들! 평소에 메모앱에다가 무엇을 적으시나요?"
+            $0.alpha = 0
+        }
+    }()
+    
 }
 
 extension InteractDetailViewController: UITableViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let alpha = scrollView.contentOffset.y / titleLabel.frame.maxY
-        guard let titleView = navigationItem.titleView else {return}
-        UIView.animate(withDuration: 0.25) {
-            titleView.alpha = (alpha < 0.8) ? 0 : 1
-        }
+        naviTitleShowing(scrollView)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "DRDetailCommentSection") as! DRDetailCommentSection
+        view.nameLabel.text = "박요진"
+        view.contentLabel.text = "보통 메모를 하거나 일기를 쓰는데 일년 전 글이 다시 올라오는게 있으면 좋을 것 같아요!"
+        view.replyLabel.text = "답글 1개"
+        view.timeLabel.text = "30분 전"
+        return view
     }
     
 }
 
 extension InteractDetailViewController: UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DRDetailCommentCell") as! DRDetailCommentCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DRDetailReplyCell") as! DRDetailReplyCell
+        cell.nameLabel.text = "Soyeon Shin"
+        cell.contentLabel.text = "Soyeon Shin 메모앱에서 그림그리는 건 정말 기본적이라서 불편한데 그 중에서도 브러쉬 크기를 못 바꾸는 게 제일 불편해요ㅠㅠ 그리고 메모 잠금하면 내용 찾기가 안되는 것도 불편하구요..."
+        cell.replyLabel.text = "답글 2개"
+        cell.timeLabel.text = "1시간 전"
         return cell
     }
     
